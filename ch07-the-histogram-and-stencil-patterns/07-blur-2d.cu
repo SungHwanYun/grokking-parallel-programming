208778b3b5b933ecdc4941e3c77355096a7d5c4c
MenOfPassion <36258098+SungHwanYun@users.noreply.github.com>
2026-05-15 07:04:42 +0900

    Change thread and block dimensions for blur2D kernel

--- ch07-the-histogram-and-stencil-patterns/07-blur-2d.cu
+++ ch07-the-histogram-and-stencil-patterns/07-blur-2d.cu
@@ -41,8 +41,8 @@ int main() {
                cudaMemcpyHostToDevice);
     cudaMemset(d_output, 0.0, n * m * sizeof(float));
  
-    dim3 threadsPerBlock(32, 32);
-    dim3 blocks((n + 32 - 1) / 32, (n + 32 - 1) / 32);
+    dim3 threadsPerBlock(16, 16);
+    dim3 blocks((n + 15) / 16, (n + 15) / 16);
     blur2D<<<blocks, threadsPerBlock>>>(d_input, d_output, n, m);
  
     cudaMemcpy(&h_output, d_output, n * m * sizeof(int),
