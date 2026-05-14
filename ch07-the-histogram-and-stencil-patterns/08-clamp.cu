213dbd1e4dd2288e9f7991a7d9433faef4c46551
MenOfPassion <36258098+SungHwanYun@users.noreply.github.com>
2026-05-15 07:17:00 +0900

    Fix block dimensions for blur2D kernel launch

--- ch07-the-histogram-and-stencil-patterns/08-clamp.cu
+++ ch07-the-histogram-and-stencil-patterns/08-clamp.cu
@@ -45,7 +45,7 @@ int main() {
     cudaMemset(d_output, 0.0, n * m * sizeof(float));
  
     dim3 threadsPerBlock(16, 16);
-    dim3 blocks((n + 15) / 16, (n + 15) / 16);
+    dim3 blocks((m + 15) / 16, (n + 15) / 16);
     blur2D<<<blocks, threadsPerBlock>>>(d_input, d_output, m, n);
  
     cudaMemcpy(&h_output, d_output, n * m * sizeof(int),
